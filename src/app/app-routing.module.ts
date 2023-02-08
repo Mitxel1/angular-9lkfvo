f628403f7aa1b6225ccc0e58f7f7c11dcee4c9bb
Mitxel1 <106549049+Mitxel1@users.noreply.github.com>
2023-02-08 12:25:38 -0600

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
-  { path: '/inicio', component: InicioComponent, pathMatch: 'full' },
-  { path: '/login', component: LoginComponent, pathMatch: 'full' },
-  { path: '/registrar', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'game/inicio', component: InicioComponent, pathMatch: 'full' },
+  { path: 'game/login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'game/registrar', component: RegisterComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
