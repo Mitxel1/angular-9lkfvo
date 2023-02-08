cc2466d35f01e4cc7a932e2b5d98ad7c67e320f9
Mixtel1 <navamiguel416@gmail.com>
2023-02-08 12:34:41 -0600

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
-  {path:'game/inicio', component:InicioComponent, pathMatch:'full'},
+  {path:'', component:InicioComponent, pathMatch:'full'},
   {path:'game/login', component:LoginComponent, pathMatch:'full'},
   {path:'game/registrar', component:RegisterComponent, pathMatch: 'full'}
 ];  
