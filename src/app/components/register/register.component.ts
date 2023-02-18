a82ad0ae9e4a9e100462cd2ecb2cd16146326b23
Mitxel1 <106549049+Mitxel1@users.noreply.github.com>
2023-02-17 20:11:26 -0600

    3 files modified

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -4,29 +4,30 @@ import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  email:string; //Captura de email
-  password:string;  //Captura de password
-  confirmPassword:string;
+  email: string; //Captura de email
+  password: string; //Captura de password
+  confirmPassword: string;
   passwordError: boolean;
 
-  constructor(public userService: UsersService) { //Inicializar los dos atributos
+  constructor(public userService: UsersService) {
     this.email = '';
-    this.password = ''
+    this.password = '';
     this.confirmPassword = '';
     this.passwordError = false;
   }
 
-  register() {  //Función para evento clic
+  register() {
+    //Función para evento clic
     console.log('Email: ' + this.email);
     console.log('Password: ' + this.password);
     console.log('Confirm Password: ' + this.confirmPassword);
 
-    const user = {email: this.email, password:this.password};
+    const user = { email: this.email, password: this.password };
 
-    this.userService.register(user).subscribe(data => {
+    this.userService.register(user).subscribe((data) => {
       console.log(data);
     });
   }
